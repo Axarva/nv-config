8e46de07a887dbcb5481c6eaf1ef18db002e0390
Axarva <timsinaatharva@gmail.com>
2022-07-15 23:20:09 +0545

    Add final keybind

--- mappings.lua
+++ mappings.lua
@@ -5,5 +5,10 @@ M.ctrlp = {
       ["<C-p>"] = {"<cmd> Telescope find_files <CR>", "Fuzzy find files"},
    },
 }
+M.ctrlo = {
+   n = {
+      ["<C-o>"] = {"<cmd> Telescope media_files <CR>", "Fuzzy find media files"},
+   },
+}
 
 return M
