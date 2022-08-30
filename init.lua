6e63d463aecbb587e3e6f030c71b8abb72f7529e
Axarva <timsinaatharva@gmail.com>
2022-08-30 13:42:39 +0545

    Add wrapper for kitty and modify lsp setup

--- init.lua
+++ init.lua
@@ -1,8 +1,38 @@
-vim.cmd [[
-augroup kitty_mp
-    autocmd!
-    au VimLeave * :silent !kitty @ set-spacing padding=15
-    au VimEnter * :silent !kitty @ set-spacing padding=0
-augroup END
-]]
+local set_spacing = function(padding_amount, margin_amount)
+    -- local command = string.format('kitty @ set-spacing padding=%d margin=%d', padding_amount, margin_amount)
+    -- vim.schedule(function()
+    --     local handle = io.popen(command)
+    --     if handle ~= nil then
+    --         handle:close()
+    --     end
+    -- end)
+  vim.schedule(function()
+      require("plenary.job"):new({
+          detached = true,
+          command = "kitty",
+          args = {
+              "@", "set-spacing",
+              "padding=" .. padding_amount,
+              "margin=" .. margin_amount,
+          },
+          cwd = "/usr/bin/",
+          }):start()
+  end)
+end
+
+local kitty_test = vim.api.nvim_create_augroup("KittyPadding", {clear = true})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = kitty_test,
+    callback = function()
+        set_spacing(0, 0)
+    end
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+    group = kitty_test,
+    callback = function()
+        set_spacing(15, 0)
+    end
+})
 
