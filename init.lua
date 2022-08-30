5c45b5402cc2267a2ab927d9d9488fd7c4223112
Axarva <timsinaatharva@gmail.com>
2022-08-30 15:10:48 +0545

    Modify init.lua

--- init.lua
+++ init.lua
@@ -1,38 +1,56 @@
+-- local set_spacing = function(padding_amount, margin_amount)
+--   vim.schedule(function()
+--       require("plenary.job"):new({
+--           detached = true,
+--           command = "kitty",
+--           args = {
+--               "@", "set-spacing",
+--               "padding=" .. padding_amount,
+--               "margin=" .. margin_amount,
+--           },
+--           cwd = "/usr/bin/",
+--           }):start()
+--   end)
+-- end
+--
+-- local kitty_test = vim.api.nvim_create_augroup("KittyPadding", {clear = true})
+--
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--     group = kitty_test,
+--     callback = function()
+--         set_spacing(0, 0)
+--     end
+-- })
+--
+-- vim.api.nvim_create_autocmd("VimLeavePre", {
+--     group = kitty_test,
+--     callback = function()
+--         set_spacing(15, 0)
+--     end
+-- })
+--
+
 local set_spacing = function(padding_amount, margin_amount)
-    -- local command = string.format('kitty @ set-spacing padding=%d margin=%d', padding_amount, margin_amount)
-    -- vim.schedule(function()
-    --     local handle = io.popen(command)
-    --     if handle ~= nil then
-    --         handle:close()
-    --     end
-    -- end)
-  vim.schedule(function()
-      require("plenary.job"):new({
-          detached = true,
-          command = "kitty",
-          args = {
-              "@", "set-spacing",
-              "padding=" .. padding_amount,
-              "margin=" .. margin_amount,
-          },
-          cwd = "/usr/bin/",
-          }):start()
-  end)
+    require("plenary.job"):new({
+        detached = true,
+        command = "kitty",
+        args = {
+            "@", "set-spacing",
+            "padding=" .. padding_amount,
+            "margin=" .. margin_amount,
+        },
+        cwd = "/usr/bin/",
+    }):start()
 end
 
 local kitty_test = vim.api.nvim_create_augroup("KittyPadding", {clear = true})
 
-vim.api.nvim_create_autocmd("VimEnter", {
-    group = kitty_test,
-    callback = function()
-        set_spacing(0, 0)
-    end
-})
+vim.schedule(function()set_spacing("0", "0") end)
 
-vim.api.nvim_create_autocmd("VimLeavePre", {
+vim.api.nvim_create_autocmd("VimLeave", {
     group = kitty_test,
     callback = function()
-        set_spacing(15, 0)
+        set_spacing("15", "0")
     end
 })
 
